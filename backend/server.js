f45771e9630e3bfe597e44f0cdfdec268ceed937
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-10 15:35:59 +0500

    add model ,route but some error

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-
 const app = express();
- const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
- app.use(cors());
- app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 })
 
+const exercisesRouter = require('./routes/exercise.js');
+const usersRouter = require('./routes/user.js');
 
-//const uri = process.env.ATLAS_URI;
-const uri = process.env.MOGOOSEDB_URI;
-mongoose.connect(uri,{useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true}) .then(() => {
-        console.log('Database connected!')
-      })
-      .catch(err => {
-        console.log('Could not connect', err.message)
-      })
-
-// const connection =mongoose.connection;
-// connection.once('open',()=> console.log('connection statblish'))
-
-
-
-
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
