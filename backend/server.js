bcae471bd02e6c461dd807b10b9ee876e2315815
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-08-10 14:04:24 +0500

    coneection the database first old code and second is new code and dot env file create add key

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+require('dotenv').config();
+
+
+const app = express();
+ const port = process.env.PORT || 5000;
+
+ app.use(cors());
+ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+//const uri = process.env.ATLAS_URI;
+const uri = process.env.MOGOOSEDB_URI;
+mongoose.connect(uri,{useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true}) .then(() => {
+        console.log('Database connected!')
+      })
+      .catch(err => {
+        console.log('Could not connect', err.message)
+      })
+
+// const connection =mongoose.connection;
+// connection.once('open',()=> console.log('connection statblish'))
+
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
